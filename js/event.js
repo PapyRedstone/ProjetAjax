9f95c7cfb51fc954c0913e3d19f802acff18f079
PapyRedstone <febvrealex@yahoo.fr>
2018-03-28 10:15:26 +0200

    modifié :         js/event.js

--- js/event.js
+++ js/event.js
@@ -9,17 +9,10 @@ window.onload = function (){
     //En cours de test
 
     //OK
-<<<<<<< HEAD
     //ajaxRequest('POST','/php/request.php/comment',callback,'login=alex&text=SLT&id_photo=1');
     //ajaxRequest('PUT','/php/request.php/comment/1',callback,'login=alex&text=COUCOOU');
     //ajaxRequest('GET','/php/request.php/image',callback);
     //ajaxRequest('GET','/php/request.php/image/1',callback);
     ajaxRequest('GET','/php/request.php/comment/1',callback);
-=======
-    //ajaxRequest('PUT','/php/request.php/comment/1',callback,'login=alex&text=COUCOOU');
-    ajaxRequest('GET','/php/request.php/image',callback);
-    ajaxRequest('GET','/php/request.php/image/1',callback);
-    ajaxRequest('GET','/php/request.php/comment/0',callback);
->>>>>>> e119381faf54367b178ac54902fea533c083283d
 }
 
